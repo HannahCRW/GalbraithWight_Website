4dde5473242ad35e14b4fd1cded73f93093a4938
Hannah <hannah_wight@hotmail.com>
2014-09-16 12:28:56 +0100

    added User controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+
+	has_secure_password validations: false
+	validates :password, presence: true, length: { minimum: 6 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
 end
