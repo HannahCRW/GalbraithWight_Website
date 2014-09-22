049335bce9be4253a88063e940cdba52348afc23
Hannah <hannah_wight@hotmail.com>
2014-09-22 15:19:32 +0100

    added some validations to email and password, realised i need to be working in 'create' not 'new'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
-
-	has_secure_password validations: false
+	attr_accessor :email, :password, :password_confirmation
+	VALID_EMAIL_REGEX = /([\w+.]+)@[a-z0-9\-.]+\.[a-z]+/i 
+	validates :email, presence: true, 
+					  format: { with: VALID_EMAIL_REGEX }, 
+					  uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 6 }
+	validates :password_confirmation, presence: true
 
 
 end
