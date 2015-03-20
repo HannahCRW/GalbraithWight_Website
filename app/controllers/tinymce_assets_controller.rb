3ed6be701949f3092cc09955c4f671957cf5bbe5
cameronolivier <cameronolivier@gmail.com>
2015-03-20 15:12:05 +0200

    Testing Uploads

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -2,14 +2,8 @@ class TinymceAssetsController < ApplicationController
   respond_to :json
 
   def create
-    
-    #geometry = Paperclip::Geometry.from_file params[:file]
-    #image    = Image.create params.permit(:file, :alt, :hint)
-
     render json: {
-      image: {
-        url:  URI.join(request.protocol, request.host_with_port) #URI.join(request.protocol, request.host_with_port, image.file.url),
-      }
+      text: "Testing."
     }, layout: false, content_type: "text/html"
   end
 end
