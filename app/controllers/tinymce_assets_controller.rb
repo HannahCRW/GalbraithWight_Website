4bab6581ebc6db9acdfc43f672cb2ce72c23aff7
cameronolivier <cameronolivier@gmail.com>
2015-03-20 15:32:45 +0200

    Adding in Image call

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -4,9 +4,10 @@ class TinymceAssetsController < ApplicationController
   def create
 
     geometry = Paperclip::Geometry.from_file params[:file]
+    image    = Image.create params.permit(:file, :alt, :hint)
 
     render json: {
-      text: "Adding in Paperclip"
+      text: "Adding in Image"
     }, layout: false, content_type: "text/html"
   end
 end
