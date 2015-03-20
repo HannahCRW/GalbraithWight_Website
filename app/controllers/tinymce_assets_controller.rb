bb24b3f86605d97683bd2a818640514a4d038839
cameronolivier <cameronolivier@gmail.com>
2015-03-20 13:23:46 +0200

    Fixing TinyMCE Image uploads

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -2,14 +2,16 @@ class TinymceAssetsController < ApplicationController
   respond_to :json
 
   def create
-    geometry = Paperclip::Geometry.from_file params[:file]
-    image    = Image.create params.permit(:file, :alt, :hint)
+    
+#    geometry = Paperclip::Geometry.from_file params[:file]
+#    image    = Image.create params.permit(:file, :alt, :hint)
 
     render json: {
-      image: {
-        url:    image.file.url,
-        height: geometry.height.to_i,
-        width:  geometry.width.to_i
+#      image: {
+#        url:    image.file.url,
+#        height: geometry.height.to_i,
+#        width:  geometry.width.to_i
+        text: "Testing"
       }
     }, layout: false, content_type: "text/html"
   end
