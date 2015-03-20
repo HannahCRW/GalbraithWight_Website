d9eb4a743a626840c0280b1ded1ae00c487386a7
cameronolivier <cameronolivier@gmail.com>
2015-03-20 15:26:50 +0200

    Testing worked. Adding back Paperclip call

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -2,8 +2,11 @@ class TinymceAssetsController < ApplicationController
   respond_to :json
 
   def create
+
+    geometry = Paperclip::Geometry.from_file params[:file]
+
     render json: {
-      text: "Testing."
+      text: "Adding in Paperclip"
     }, layout: false, content_type: "text/html"
   end
 end
