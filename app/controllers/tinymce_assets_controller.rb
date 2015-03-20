b36c6bce16c5eb25d92c2d0336f90f11043d6822
cameronolivier <cameronolivier@gmail.com>
2015-03-20 14:21:47 +0200

    Testing Controller - finding what throws the error

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -3,15 +3,16 @@ class TinymceAssetsController < ApplicationController
 
   def create
     
-    geometry = Paperclip::Geometry.from_file params[:file]
-    image    = Image.create params.permit(:file, :alt, :hint)
+   # geometry = Paperclip::Geometry.from_file params[:file]
+   # image    = Image.create params.permit(:file, :alt, :hint)
 
     render json: {
-      image: {
-        url:  URI.join(request.protocol, request.host_with_port, image.file.url),
-        height: geometry.height.to_i,
-        width:  geometry.width.to_i
-      }
+     # image: {
+    #    url:  URI.join(request.protocol, request.host_with_port, image.file.url),
+    #    height: geometry.height.to_i,
+    #    width:  geometry.width.to_i
+    #  }
+    text : "Testing"
     }, layout: false, content_type: "text/html"
   end
 end
