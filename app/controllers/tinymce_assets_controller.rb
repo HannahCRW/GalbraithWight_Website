7701adee97ce8693cfe303ba883d88d948f77622
cameronolivier <cameronolivier@gmail.com>
2015-03-20 14:29:05 +0200

    Testing Tinymce Assets Controller

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -3,14 +3,12 @@ class TinymceAssetsController < ApplicationController
 
   def create
     
-    geometry = Paperclip::Geometry.from_file params[:file]
-    image    = Image.create params.permit(:file, :alt, :hint)
+    #geometry = Paperclip::Geometry.from_file params[:file]
+    #image    = Image.create params.permit(:file, :alt, :hint)
 
     render json: {
       image: {
-        url:  URI.join(request.protocol, request.host_with_port, image.file.url),
-        height: geometry.height.to_i,
-        width:  geometry.width.to_i
+        url:  URI.join(request.protocol, request.host_with_port) #URI.join(request.protocol, request.host_with_port, image.file.url),
       }
     }, layout: false, content_type: "text/html"
   end
