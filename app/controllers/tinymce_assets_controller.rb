d41dd792794ea835daf7f90e98c7387df4b1ded5
cameronolivier <cameronolivier@gmail.com>
2015-03-23 11:48:26 +0200

    testing tinymcassetscontroller

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -3,13 +3,12 @@ class TinymceAssetsController < ApplicationController
 
   def create
     geometry = Paperclip::Geometry.from_file params[:file]
-    image    = Image.create params.permit(:file, :alt, :hint)
+    
 
     render json: {
-      image: {
-        url:    image.file.url,
-        height: geometry.height.to_i,
-        width:  geometry.width.to_i
+      avar: {
+        text: "Back to Testing",
+        url:    image.file.url 
       }
     }, layout: false, content_type: "text/html"
   end
