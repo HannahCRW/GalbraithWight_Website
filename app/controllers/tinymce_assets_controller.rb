3803af1ffe3714779cf8368b24c51ded9e560ca3
cameronolivier <cameronolivier@gmail.com>
2015-03-23 11:35:42 +0200

    hopefully fixing images

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -2,14 +2,14 @@ class TinymceAssetsController < ApplicationController
   respond_to :json
 
   def create
-
     geometry = Paperclip::Geometry.from_file params[:file]
-    image    = Image.create params
+    image    = Image.create params.permit(:file, :alt, :hint)
 
     render json: {
-      url:  URI.join(request.protocol, request.host_with_port, image.file.url),
-      height: geometry.height.to_i,
-      width:  geometry.width.to_i
+      image: {
+        url:    image.file.url,
+        height: geometry.height.to_i,
+        width:  geometry.width.to_i
+      }
     }, layout: false, content_type: "text/html"
-  end
 end
